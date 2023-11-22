501ec3f0f118e225c1bb075c2aa8af59dd902d94
honnuanand <anand@honnu.com>
2023-11-22 15:24:24 -0800

    base properties for Claude2 and Jurasic2 are completed.

--- Readme.md
+++ Readme.md
@@ -29,7 +29,7 @@ aws.bedrock.model.claude.stopSequences=\n\nHuman:
 ```properties 
 aws.bedrock.model.id=jurassic2
 aws.bedrock.model.jurassic.modelId=ai21.j2-mid-v1
-<!-- aws.bedrock.model.jurassic.prompt=Your prompt here -->
+aws.bedrock.model.jurassic.prompt=Your prompt here
 aws.bedrock.model.jurassic.maxTokens=200
 aws.bedrock.model.jurassic.temperature=0.5
 ```
--- src/main/java/io/clue2solve/aws/bedrock/springboot/starter/config/JurassicProperties.java
+++ src/main/java/io/clue2solve/aws/bedrock/springboot/starter/config/JurassicProperties.java
@@ -4,13 +4,12 @@ package io.clue2solve.aws.bedrock.springboot.starter.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-
-@ConfigurationProperties(prefix = "aws.bedrock.model.jurassic")
-public record JurassicProperties(String modelId, String prompt, int maxTokens, double temperature) {}
-
 /**Sample perperties
  * aws.bedrock.model.jurassic.modelId=arn:aws:sagemaker:us-east-1:123456789012:model/jurassic-park
  * aws.bedrock.model.jurassic.prompt=Human: Assistant:
  * aws.bedrock.model.jurassic.maxTokens=200
  * aws.bedrock.model.jurassic.temperature=0.5
  */
+@ConfigurationProperties(prefix = "aws.bedrock.model.jurassic")
+public record JurassicProperties(String modelId, String prompt, int maxTokens, double temperature) {}
+
