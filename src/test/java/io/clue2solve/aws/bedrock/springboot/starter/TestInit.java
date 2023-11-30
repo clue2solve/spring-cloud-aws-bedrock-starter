7d037b7dabb541ae3126e90ecbc38187106a80f6
Chris Phillipson <cphillipson@vmware.com>
2023-11-30 06:14:21 -0800

    Move auto-config to META-INF/spring * and remove @ImportAutoConfiguration from TestInit

--- src/test/java/io/clue2solve/aws/bedrock/springboot/starter/TestInit.java
+++ src/test/java/io/clue2solve/aws/bedrock/springboot/starter/TestInit.java
@@ -1,11 +1,8 @@
 package io.clue2solve.aws.bedrock.springboot.starter;
 
-import io.clue2solve.aws.bedrock.springboot.starter.autoconfigure.aimodels.BedrockAutoConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ImportAutoConfiguration(BedrockAutoConfiguration.class)
 public class TestInit {
 
 }
