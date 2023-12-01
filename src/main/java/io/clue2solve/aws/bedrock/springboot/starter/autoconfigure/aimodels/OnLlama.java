4c59a1a82256fc9bdb65beb1760bee893aac6b5d
honnuanand <anand@honnu.com>
2023-11-30 17:10:00 -0800

    Spring Formatting - Change

--- src/main/java/io/clue2solve/aws/bedrock/springboot/starter/autoconfigure/aimodels/OnLlama.java
+++ src/main/java/io/clue2solve/aws/bedrock/springboot/starter/autoconfigure/aimodels/OnLlama.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 class OnLlama extends AnyNestedCondition {
 
-    /**
+	/**
 	 * Constructor
 	 */
 	OnLlama() {
 		super(ConfigurationPhase.PARSE_CONFIGURATION);
 	}
 
-    /**
+	/**
 	 * Condition for Llama meta.llama2-13b-chat-v1
 	 */
 	@ConditionalOnProperty(name = "aws.bedrock.model.llama2.id", havingValue = "meta.llama2-13b-chat-v1")
